747a2e2e932646bf49297b70e2fea5804ee0ce8a
Vladimir Chernov <v.chernov@zebrains.team>
2021-04-26 15:47:49 +0300

    Вношу правки

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <div className="nav-wrapper">
       <a href="/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="https://github.com/VladimirChernovv/Project_shop" target="_blank" rel="noreferror">Repo</a></li>
+        <li><a href="https://github.com/VladimirChernovv/Project_shop" target="_blank">Repo</a></li>
       </ul>
     </div>
   </nav>
