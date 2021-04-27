d9c51e8dfbb8bb03402f4da441c8194949acc7c1
Vladimir Chernov <v.chernov@zebrains.team>
2021-04-27 12:33:11 +0300

    Создание контекста с провайдером

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,3 +1,6 @@
+import {useContext} from 'react';
+import {ShopContext} from '../context';
+
 function BasketItem(props) {
   const {
     mainId,
@@ -8,6 +11,9 @@ function BasketItem(props) {
     incrementQuantity = Function.prototype,
     decrementQuantity = Function.prototype,
   } = props;
+
+  const {exemple} = useContext(ShopContext);
+  console.log(exemple);
 
   return (
     <li className="collection-item">
